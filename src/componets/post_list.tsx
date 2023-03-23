a65a0a8db77c008a18c75e70ec14b1b04b2df5de
N <mail@nuhhtyy.xyz>
2023-03-23 00:58:40 -0700

    gn

---
+++ src/componets/post_list.tsx
@@ -0,0 +1,14 @@
+import { _getPosts } from "@/services/getPosts";
+import { Post } from "@/pages/posts/[post]";
+
+export function PostList({ posts }: { posts: Post[] }) {
+  return (
+    <ul style={{ listStyle: "none", paddingLeft: "0" }}>
+      {posts.map((post) => (
+        <li style={{ paddingTop: ".3rem" }}>
+          <a href={`/posts/${post.slug}`}>{post.post_title}</a>
+        </li>
+      ))}
+    </ul>
+  );
+}
---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div className="about">
+      <h2 className="about-tag">about me</h2>
+      <h4 className="about-text">
+        I'm a software engineer and cryptography enjoyer currently working on
+        blockchain stuff at <a href="https://alongside.xyz">Alongside</a>. I
+        like working on backend things and things related to distributed systems
+        in general. Here you will find (potentialy incoherent) writings and info
+        dumps about things I find interesting.
+      </h4>
+    </div>
+  );
+}
