b184108116a44c6531813d201b61e1223a06544a
N <mail@nuhhtyy.xyz>
2023-05-25 22:01:54 -0400

    fix email

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,12 +2,12 @@ export default function Contact() {
   return (
     <p>
       <a
-        href="mailto:mail@ad8.dev"
+        href="mailto:n@ad8.dev"
         className="hover-red"
         style={{ fontSize: "large" }}
       >
         {" "}
-        mail@ad8.dev
+        n@ad8.dev
       </a>
     </p>
   );
