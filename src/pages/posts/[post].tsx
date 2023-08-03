0cbad73f8ab800a6d25c63de1edb6cc922eb99f1
N <mail@nuhhtyy.xyz>
2023-08-03 16:28:51 -0400

    remove weird json scheme just use markdown

--- src/pages/posts/[post].tsx
+++ src/pages/posts/[post].tsx
@@ -12,47 +12,17 @@ import rehypePrettyCode from "rehype-pretty-code";
 import remarkGfm from "remark-gfm";
 import { setCDN } from "shiki";
 
-function isImage(content: any): content is Image {
-  return (content as Image).url !== undefined;
-}
-
-function isSection(content: any): content is Section {
-  return (content as Section).title !== undefined;
-}
-
-type Image = {
-  url: string;
-};
-
-type Section = {
-  title: string;
-  content: (string | Section | Image)[];
-};
-
 export type Post = {
   post_title: string;
   slug: string;
-  sections: Section[];
-};
-
-type TableOfContents = {
-  sections: {
-    title: string;
-    subsection_titles?: string[];
-  }[];
+  content: string;
 };
 
 export default function Page(props: { post: Post }) {
   return PostRenderer(props.post);
 }
 
-function SectionContent({
-  content,
-  depth,
-}: {
-  content: string;
-  depth: number;
-}) {
+function ContentRenderer({ content }: { content: string }) {
   const [renderedContent, setRenderedContent] = useState("");
   setCDN("/");
 
@@ -81,184 +51,19 @@ function SectionContent({
     <div
       style={{
         textAlign: "left",
-        marginLeft: (depth + 1).toString() + "rem",
       }}
       dangerouslySetInnerHTML={{ __html: renderedContent }}
     />
   );
 }
 
-function Section({
-  section,
-  depth,
-  sectionRefs,
-}: {
-  section: Section;
-  depth: number;
-  sectionRefs: { [key: string]: any };
-}) {
-  const sectionRef = useRef(null);
-  const [isCollapsed, setIsCollapsed] = useState(false);
-  sectionRefs[section.title] = sectionRef;
-
-  const toggleCollapse = () => {
-    setIsCollapsed(!isCollapsed);
-  };
-
-  return (
-    <div key={section.title}>
-      <h3
-        style={{
-          textAlign: "left",
-          marginLeft: depth.toString() + "rem",
-          marginBottom: "0",
-          textDecoration: "underline",
-          cursor: "pointer",
-        }}
-        className={"hover-red"}
-        onClick={toggleCollapse}
-        ref={sectionRef}
-      >
-        {isCollapsed ? "[+] " + section.title : section.title}
-      </h3>
-      <div
-        style={{
-          maxHeight: isCollapsed ? "0" : "2000px",
-          opacity: isCollapsed ? "0" : "1",
-          overflow: "hidden",
-          transition: "max-height 0.5s ease-in-out, opacity 0.5s ease-in-out",
-        }}
-      >
-        {section.content.map((contentItem, index) => {
-          if (typeof contentItem === "string") {
-            return (
-              <SectionContent key={index} content={contentItem} depth={depth} />
-            );
-          } else if (isImage(contentItem)) {
-            return (
-              <img
-                key={index}
-                src={contentItem.url}
-                style={{ maxWidth: "100%" }}
-              />
-            );
-          } else if (isSection(contentItem)) {
-            return (
-              <Section
-                key={index}
-                section={contentItem}
-                depth={depth + 1}
-                sectionRefs={sectionRefs}
-              />
-            );
-          } else {
-            console.log("Section Rendering Error: Unknown content item type");
-            return null;
-          }
-        })}
-      </div>
-    </div>
-  );
-}
-
-function scrollToRef(ref: React.MutableRefObject<HTMLInputElement>) {
-  ref.current.scrollIntoView({ behavior: "smooth" });
-}
-
-function TableOfContentsRenderer(
-  toc: TableOfContents,
-  sectionRefs: { [key: string]: React.MutableRefObject<HTMLInputElement> }
-) {
-  return (
-    <div style={{ textAlign: "left" }}>
-      <ul style={{ listStyle: "none" }}>
-        {toc.sections.map((section) => (
-          <li key={section.title} style={{ paddingTop: ".5rem" }}>
-            <a
-              onClick={() => scrollToRef(sectionRefs[section.title])}
-              style={{
-                textDecoration: "underline",
-                cursor: "pointer",
-                fontSize: "large",
-              }}
-              className="hover-red"
-            >
-              {section.title}
-            </a>
-            {section.subsection_titles && (
-              <ul style={{ listStyle: "none" }}>
-                {section.subsection_titles.map((subsection) => (
-                  <li key={subsection}>
-                    <a
-                      onClick={() => scrollToRef(sectionRefs[subsection])}
-                      style={{
-                        textDecoration: "underline",
-                        cursor: "pointer",
-                        fontSize: "large",
-                      }}
-                      className="hover-red"
-                    >
-                      {subsection}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
 function PostRenderer(post: Post) {
-  const toc = extractTableOfContents(post);
-
-  let sectionRefs = {};
   return (
     <div style={{ paddingBottom: "10rem" }}>
       <h1 style={{ textAlign: "center" }}>{post.post_title}</h1>
-      {TableOfContentsRenderer(toc, sectionRefs)}
-      {post.sections.map((section, index) => (
-        <Section
-          key={index}
-          section={section}
-          depth={0}
-          sectionRefs={sectionRefs}
-        />
-      ))}
+      <ContentRenderer content={post.content} />
     </div>
   );
-}
-
-function extractTableOfContents(post: Post): TableOfContents {
-  const tableOfContents: TableOfContents = {
-    sections: [],
-  };
-
-  const extractSubsections = (section: Section): string[] => {
-    const subsectionTitles: string[] = [];
-
-    section.content.forEach((item) => {
-      if (isSection(item)) {
-        subsectionTitles.push(item.title);
-        subsectionTitles.push(...extractSubsections(item));
-      }
-    });
-
-    return subsectionTitles;
-  };
-
-  post.sections.forEach((section) => {
-    const subsections = extractSubsections(section);
-    const tocSection = {
-      title: section.title,
-      subsection_titles: subsections.length > 0 ? subsections : undefined,
-    };
-    tableOfContents.sections.push(tocSection);
-  });
-
-  return tableOfContents;
 }
 
 export async function getServerSideProps(context: any) {
--- src/services/getPosts.ts
+++ src/services/getPosts.ts
@@ -1,10 +1,18 @@
-import ECDSAPost from "../assets/ecdsa_post.json";
-import RustErrs from "../assets/rust_err.json";
+// import ECDSAPost from "../assets/ecdsa_post.json";
+// import RustErrs from "../assets/rust_err.json";
 import { Post } from "../pages/posts/[post]";
+import { read } from "to-vfile";
 
 // todo: fetch from mongo db
 export const _getPosts = async () => {
-  return [ECDSAPost, RustErrs] as Post[];
+  let testmd_content = (await read("src/assets/testmd.md")).toString();
+  return [
+    {
+      post_title: "testmd",
+      slug: "testmd",
+      content: testmd_content,
+    },
+  ] as Post[];
 };
 
 export async function getPosts(): Promise<Map<string, Post>> {
