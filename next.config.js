6a546f3df93809881a55153c8d060ff2e052381d
N <mail@nuhhtyy.xyz>
2023-08-03 23:36:24 -0400

    reduce config static render

--- next.config.js
+++ next.config.js
@@ -1,43 +1,3 @@
-const getHighlighter = require("shiki").getHighlighter;
-const rehypePrettyCode = require("rehype-pretty-code");
-const rehypeStringify = import("rehype-stringify");
-const rehypeKatex = import("rehype-katex");
-const remarkMath = import("remark-math");
-const remarkImages = import("remark-images");
-const remarkParse = import("remark-parse");
-
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [remarkParse, remarkMath, remarkImages],
-    rehypePlugins: [
-      rehypeKatex,
-      rehypeStringify,
-      [
-        rehypePrettyCode,
-        {
-          theme: "github-dark-dimmed",
-          keepBackground: true,
-          getHighlighter: (opts) =>
-            getHighlighter({
-              ...opts,
-              paths: {
-                themes: "themes/",
-                wasm: "dist/",
-                languages: "languages/",
-              },
-            }),
-        },
-      ],
-    ],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure pageExtensions to include md and mdx
@@ -47,4 +7,4 @@ const nextConfig = {
 };
 
 // Merge MDX config with Next.js config
-module.exports = withMDX(nextConfig);
+module.exports = nextConfig;
--- src/services/getPosts.ts
+++ src/services/getPosts.ts
@@ -1,24 +1,19 @@
-import { Post } from "../pages/posts/[post]";
 import { readFileSync } from "fs";
 import path from "path";
 
 // todo: fetch from mongo db
 export const _getPosts = async () => {
-  let entry = path.join(process.cwd(), "posts");
-  let ecdsa_content = readFileSync(path.join(entry, "ecdsa.md")).toString();
-
   return [
     {
       post_title: "ECDSA",
       slug: "ecdsa",
-      content: ecdsa_content,
     },
-  ] as Post[];
+  ];
 };
 
-export async function getPosts(): Promise<Map<string, Post>> {
-  const posts = await _getPosts();
-  const postsMap = new Map<string, Post>();
-  posts.forEach((post) => postsMap.set(post.slug, post));
-  return postsMap;
-}
+// export async function getPosts(): Promise<Map<string, Post>> {
+//   const posts = await _getPosts();
+//   const postsMap = new Map<string, Post>();
+//   posts.forEach((post) => postsMap.set(post.slug, post));
+//   return postsMap;
+// }
