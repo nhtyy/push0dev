eebfce7518b0fbf19c89dfb7aeff5df459d7df43
N <mail@nuhhtyy.xyz>
2023-08-07 16:00:59 -0400

    use static paths instead of ssr

--- src/pages/posts/[post].tsx
+++ src/pages/posts/[post].tsx
@@ -8,22 +8,46 @@ export default function Post(props: { title: string; stringified: string }) {
   return PostRenderer(title, stringified);
 }
 
-export async function getServerSideProps(ctx: any) {
-  let slug = ctx.params.post;
+// export async function getServerSideProps(ctx: any) {
+//   let slug = ctx.params.post;
 
+//   let registry = posts();
+
+//   let slugs = Object.keys(registry);
+
+//   // check if the slug is valid post
+//   if (!slugs.includes(slug)) {
+//     return {
+//       notFound: true,
+//     };
+//   }
+
+//   let _path = path.join(process.cwd(), "posts", `${slug}.md`);
+//   let content = readFileSync(_path).toString();
+
+//   return processPost(content, registry[slug]);
+// }
+
+export async function getStaticProps({ params }: { params: any }) {
+  let content = readFileSync(
+    path.join(process.cwd(), "posts", `${params.post}.md`)
+  ).toString();
+
+  console.log(params);
+
+  return processPost(content, posts()[params.post]);
+}
+
+export async function getStaticPaths() {
   let registry = posts();
-
   let slugs = Object.keys(registry);
 
-  // check if the slug is valid post
-  if (!slugs.includes(slug)) {
-    return {
-      notFound: true,
-    };
-  }
-
-  let _path = path.join(process.cwd(), "posts", `${slug}.md`);
-  let content = readFileSync(_path).toString();
-
-  return processPost(content, registry[slug]);
+  return {
+    paths: slugs.map((slug) => ({
+      params: {
+        post: slug,
+      },
+    })),
+    fallback: false,
+  };
 }
