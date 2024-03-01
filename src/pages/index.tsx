0211b8a0696e178b8f75ae1a46404373586dc7d8
N <n@push0.dev>
2024-03-01 12:50:40 -0500

    rm contribs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,11 @@ export default function Home({ registry }: { registry: PostRegistry }) {
   return (
     <div>
       <h2 className="homepage-section-header">Writing</h2>
-      <p>None :(</p>
+      <p></p>
       <PostList registry={registry} />
       <h2 className="homepage-section-header">Contributions</h2>
       <LinkList
         links={[
-          {
-            title: "Alongside Finance",
-            description: "An on chain crypto index protocol",
-            url: "https://alongside.xyz",
-          },
           {
             title: "uniswapx-sdk-rs",
             description: "uniswapx api types, client and server written in rust",
@@ -26,21 +21,26 @@ export default function Home({ registry }: { registry: PostRegistry }) {
             description: "a uniswap v3 toolkit in rust",
             url: "https://github.com/nhtyy/v3-rs",
           },
-          {
-            title: "reth",
-            description: "a rust ethereum client",
-            url: "https://github.com/paradigmxyz/reth/commits?author=nhtyy",
-          },
-          {
-            title: "foundry",
-            description: "a solidity testing framework written in rust",
-            url: "https://github.com/foundry-rs/foundry/commits?author=nhtyy",
-          },
+          // {
+          //   title: "reth",
+          //   description: "a rust ethereum client",
+          //   url: "https://github.com/paradigmxyz/reth/commits?author=nhtyy",
+          // },
+          // {
+          //   title: "foundry",
+          //   description: "a solidity testing framework written in rust",
+          //   url: "https://github.com/foundry-rs/foundry/commits?author=nhtyy",
+          // },
           {
             title: "lightning",
             description: "a L1 Blockchain for hosting edge services",
             url: "https://github.com/fleek-network/lightning/commits?author=nhtyy",
-          }
+          },
+          {
+            title: "Alongside Finance",
+            description: "An on chain crypto index protocol",
+            url: "https://alongside.xyz",
+          },
         ]}
       />
     </div>
