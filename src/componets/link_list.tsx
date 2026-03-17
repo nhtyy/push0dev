5a785a34a7ea9bcff9b2b081e3b30fa53ed1f017
nhtyy <n@push0.dev>
2026-03-17 01:53:55 -0400

    feat: dates and titles

--- src/componets/link_list.tsx
+++ src/componets/link_list.tsx
@@ -1,18 +1,22 @@
 export function LinkList({
   links,
 }: {
-  links: { title: string; description: string, url: string }[];
+  links: { title: string; description: string, url: string, position?: string, start?: string, end?: string }[];
 }) {
   return (
-    <ul style={{ listStyle: "none", paddingLeft: "0" }}>
-      {links.map(({ title, description, url }) => (
-        <li key={title} style={{ fontSize: "2.5vh" }}>
+    <ul style={{ listStyle: "none",
+      paddingLeft: 0,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center", }}>
+      {links.map(({ title, description, url, position, start, end }) => (
+        <li key={title} style={{ width: "100%", maxWidth: "clamp(320px, 50vw, 600px)" }}>
           <a href={url} className={"hover-red"}>
-            {title}
+            <span style={{ fontSize: "2.5vh" }}>{title}</span>
           </a>
-          <br/>
-          <h5 style={{marginTop: "1vh", marginBottom: "1.5vh", fontWeight: "normal"}}>{description}</h5>
-          {/* <p style={{marginTop: "1vh", marginBottom: "1.5vh"}}>{description}</p> */}
+          {position && <div style={{ color: "gray", fontStyle: "italic"}}>{position} </div>}
+          {start && end && <div style={{ color: "gray", fontStyle: "italic" }}>{start}-{end}</div>}
+          <h5 style={{marginTop: ".5vh", marginBottom: ".5vh", opacity: 0.8, fontWeight: "normal", fontSize: "1.5vh", whiteSpace: "pre-line" }}>{description}</h5>
         </li>
       ))}
     </ul>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,35 +3,45 @@ import { LinkList } from "@/componets/link_list";
 
 export default function Home({ registry }: { registry: PostRegistry }) {
   return (
-    <div style={{justifyContent: "center"}}>
+    <div style={{ justifyContent: "center" }}>
       {/* <h2 className="homepage-section-header">About</h2> */}
       {/* <div style={{ width: "40vw", textAlign: "center", justifyContent: "center", margin: "auto" }}>
         <p style={{fontWeight: "normal", fontSize: "2vh"}}>
           mostly rust, math, ethereum, and open source
         </p>
       </div> */}
-      <h2 className="homepage-section-header">Contributions</h2>
+      <h2 className="homepage-section-header">Experience</h2>
       <LinkList
         links={[
           {
-            title: "SP1 zkVM",
-            description: "A blazingly fast zero-knowledge virtual machine",
+            title: "Symbiosis",
+            position: "Founder and CEO",
+            description: "Coming soon...",
+            url: "https://symbiosis.markets",
+            start: "Jan 2026",
+            end: "Present",
+          },
+          {
+            title: "Succinct Labs",
+            description: "While at Succinct, I implemented and deployed novel cryptographic primitives as well focused on system design and optimization, including a custom RISC-V just-in-time compiled exeuction engine. \n\n My team and I were the first to achieve a full execution proof of a mainnet Ethereum block in under 12 seconds. Today, SP1 secures millions of dollars in user funds across multiple chains.",
             url: "https://github.com/succinctlabs/sp1",
+            start: "Nov 2024",
+            end: "Nov 2025",
+            position: "Research Engineer",
           },
           {
             title: "Alongside Finance",
-            description: "An on-chain crypto index protocol",
+            description: "At Alongside, I spearheaded design and development of both the V1 and V2 smart contract protocols. Besides implementation, my work and research focused on novel rebalancing designs and the security of the overall system.",
             url: "https://alongside.xyz",
+            position: "Smart Contract Engineer",
+            start: "Feb 2021",
+            end: "Dec 2022",
           },
           {
-            title: "Sentiment V2",
-            description: "On-chain leverage lending and borrowing",
+            title: "Sentiment",
+            description: "During my engagment with Sentiment, I helped build and design the V2 smart contract protocol on Hyperliquid. Most of my work involed implementation of the core risk engine, which was in charge of the solvency of the entire system, securing over $40M in user funds.",
+            position: "Smart Contract Engineer",
             url: "https://github.com/sentimentxyz/protocol-v2",
-          },
-          {
-            title: "Lightning",
-            description: "An L1 Blockchain for hosting edge services",
-            url: "https://github.com/fleek-network/lightning",
           },
         ]}
       />
